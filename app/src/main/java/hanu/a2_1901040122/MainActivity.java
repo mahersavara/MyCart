70549084c3332cf718d648c32e8a80b5815fbb43
mahersevara <kdlinh8811@gmail.com>
2022-04-24 16:58:37 +0700

    AddingCart iccon & Customize the menu for project

--- app/src/main/java/hanu/a2_1901040122/MainActivity.java
+++ app/src/main/java/hanu/a2_1901040122/MainActivity.java
@@ -3,6 +3,7 @@ package hanu.a2_1901040122;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
     }
+
+    // method to inflate the options menu when
+    // the user opens the menu for the first time
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
